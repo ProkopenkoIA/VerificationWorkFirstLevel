8092695a2bf102125503dd155d46521f0fb04030
ProkopenkoIA <boroda5384@mail.ru>
2023-03-12 21:02:39 +0500

    Коммит 4: Добавление проекта

---
+++ Project/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.Clear();
+
+string[] CheckElement(string[] array)
+{
+    string[] result = new string[] { };
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            Array.Resize(ref result, result.Length + 1);
+            result[result.Length - 1] = array[i];
+        }
+    }
+
+    if (result.Length == 1)
+    {
+        result[result.Length - 1] = "Нет рузультата!";
+    }
+    return result;
+}
+
+
+string consoleString = string.Empty;
+string[] consoleArray;
+string[] resultArray;
+Console.WriteLine("Введите исходную строку через пробел и нажмите Enter...");
+consoleString = Console.ReadLine();
+
+if (consoleString != null)
+{
+    consoleArray = consoleString.Split(new Char[] { ' ' });
+    resultArray = CheckElement(consoleArray);
+    Console.WriteLine($"Результат: [{string.Join(", ", resultArray)}]");
+}
+
+
